e1948bec7dae6afd364c5f71b9885073bf63248f
alonronin <alon@ronin.co.il>
2020-12-12 22:50:34 +0200

    :sparkle: new home with some useful links and information

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,26 +1,71 @@
-import { useEffect, useState } from 'react';
-
-import { getPosts } from './service.js';
-
-const Post = ({ title, body }, key) => (
-  <li className="prose prose-gray lg:prose-xl" key={key}>
-    <h2>{title}</h2>
-    <p>{body}</p>
-  </li>
-);
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    getPosts().then(setPosts).catch(console.error);
-  }, []);
-
   return (
-    <div className="container m-auto p-4">
-      <h1 className="text-gray-700 text-5xl font-bold my-4">Posts</h1>
+    <div className="prose prose-blue lg:prose-xl">
+      <h1>React, Tailwind and Webpack 5 Boilerplate</h1>
 
-      <ul className="space-y-4">{posts.map(Post)}</ul>
+      <p>
+        A bare minimum, and as much as un-opinionated as can be, boilerplate so
+        you can start develope and deploy React apps with Tailwind CSS and
+        Webpack 5.
+      </p>
+
+      <p>
+        Checkout <Link to="/posts">Posts</Link> page.
+      </p>
+
+      <h2>Resources</h2>
+
+      <ul>
+        <li>
+          <a href="https://reactjs.com" target="_blank">
+            React
+          </a>
+
+          <ul>
+            <li>
+              <a href="https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#whats-a-jsx-transform">
+                React 17 JSX Transform
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://tailwindcss.com" target="_blank">
+            Tailwind CSS
+          </a>
+
+          <ul>
+            <li>
+              <a href="https://tailwindcss.com/docs/typography-plugin">
+                @tailwindcss/typography
+              </a>
+            </li>
+
+            <li>
+              <a href="https://tailwindcss-forms.vercel.app/">
+                @tailwindcss/forms
+              </a>
+            </li>
+
+            <li>
+              <a href="https://github.com/tailwindlabs/tailwindcss-aspect-ratio">
+                @tailwindcss/aspect-ratio
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="https://reactrouter.com/">React Router</a>
+        </li>
+
+        <li>
+          <a href="https://webpack.js.org/">Webpack</a>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/home/route.jsx
+++ src/home/route.jsx
@@ -1,5 +1,6 @@
 import { lazy } from 'react';
 
 export const path = '/';
+export const title = 'Home';
 export const exact = true;
 export const component = lazy(() => import('./Home.jsx'));
