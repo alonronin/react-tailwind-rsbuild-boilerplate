e64048bfc3c08581e8c0ac089029f6a05a6cc90b
alonronin <alon@ronin.co.il>
2024-07-24 00:22:56 +0300

    :recycle: remove webpack and change to rsbuild

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="prose prose-blue lg:prose-xl">
-      <h1>React, Tailwind and Webpack 5 Boilerplate</h1>
+      <h1>React, Tailwind and RSBuild Boilerplate</h1>
 
       <p>
         A bare minimum, and as much as un-opinionated as can be, boilerplate so
-        you can start develope and deploy React apps with Tailwind CSS and
-        Webpack 5.
+        you can start develop and deploy React apps with Tailwind CSS and
+        RSBuild.
       </p>
 
       <p>
