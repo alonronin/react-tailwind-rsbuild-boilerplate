20e4fb331c47d54a1dc6d8cb205ec0e340fa93a1
alonronin <alon@ronin.co.il>
2024-07-26 01:23:19 +0300

    :recycle: rules of react, don't call a component as a function.

--- src/posts/Posts.tsx
+++ src/posts/Posts.tsx
@@ -2,18 +2,19 @@ import { useEffect, useState } from 'react';
 
 import { getPosts } from './service.ts';
 
-const Post = (
-  { title, body }: { title: string; body: string },
-  key: number,
-) => (
-  <div className="prose lg:prose-xl" key={key}>
-    <h2>{title}</h2>
-    <p>{body}</p>
-  </div>
-);
+type Post = { title: string; body: string };
 
-const Posts = () => {
-  const [posts, setPosts] = useState([]);
+function Post({ title, body }: Post) {
+  return (
+    <div className="prose lg:prose-xl">
+      <h2>{title}</h2>
+      <p>{body}</p>
+    </div>
+  );
+}
+
+export default function Posts() {
+  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     getPosts().then(setPosts).catch(console.error);
@@ -23,9 +24,11 @@ const Posts = () => {
     <div className="prose prose-blue lg:prose-xl">
       <h1>Posts</h1>
 
-      <div className="space-y-4">{posts.map(Post)}</div>
+      <div className="space-y-4">
+        {posts.map((props, key) => (
+          <Post key={key} {...props} />
+        ))}
+      </div>
     </div>
   );
-};
-
-export default Posts;
+}
